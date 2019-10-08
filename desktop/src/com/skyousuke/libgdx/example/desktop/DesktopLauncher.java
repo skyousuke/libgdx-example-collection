d096c5fc8fbf2d1152b104eda53dde36e187d91a
Surasek Nusati <surasek@gmail.com>
2019-10-08 14:55:45 +0700

    First Commit

---
+++ desktop/src/com/skyousuke/libgdx/example/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skyousuke.libgdx.example.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skyousuke.libgdx.example.GdxExampleCollection;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxExampleCollection(), config);
+	}
+}
